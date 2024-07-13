e580d7a6d9a12cfab60a0e002c629d433b6a582e
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-07-13 15:16:42 +0800

    Installed libraries

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HomePage from "./pages/HomePage"
+import MenuPage from "./pages/MenuPage"
+
+function App() {
+
+
+  return (
+    <div className="w-screen h-screen overflow-x-hidden">
+      <MenuPage/>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/MenuPage.jsx
@@ -0,0 +1,15 @@
+
+
+const links = [{
+    path: "/home", label: "Home"
+},{path: "/home", label: "Home"},{path: "/home", label: "Home"},{path: "/home", label: "Home"}]
+
+const MenuPage = () => {
+  return (
+    <div className="h-full w-full">
+        
+    </div>
+  )
+}
+
+export default MenuPage
