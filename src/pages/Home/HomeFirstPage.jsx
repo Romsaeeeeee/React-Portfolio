1a228d7788fe3ef7783a1f57674ae4425453c14a
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-07-13 17:06:53 +0800

    HomePage first page responsiveness

--- src/pages/Home/HomeFirstPage.jsx
+++ src/pages/Home/HomeFirstPage.jsx
@@ -3,11 +3,13 @@ import myImage from "../../assets/IMG_20200808_195723_565.jpg";
 
 const HomeFirstPage = () => {
   return (
-    <div className="h-full w-full flex flex-col gap-10 justify-center items-center bg-black text-white">
+    <div className="h-full w-full flex flex-col gap-8 justify-center items-center bg-black text-white">
       <img src={myImage} className="h-80 rounded-full"></img>
-      <div className="flex flex-col justify-center items-center ">
-        <p className="text-6xl ">JAYCEE BAUTISTA</p>
-        <p className="text-2xl tracking-[15px]">FRONT-END DEVELOPER</p>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-6xl tablet:text-5xl">JAYCEE BAUTISTA</p>
+        <p className="text-2xl tracking-[15px] tablet:text-xl tablet:tracking-[10px]">
+          FRONT-END DEVELOPER
+        </p>
       </div>
     </div>
   );
