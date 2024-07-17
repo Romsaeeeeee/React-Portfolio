a13d869ee19a65cc0d0dac94c45b75f37f69d35f
Romsaeeeeee <jayceebautista0929@gmail.com>
2024-07-17 15:25:38 +0800

    Footer Modal & responsiveness

--- src/modal/Footer.jsx
+++ src/modal/Footer.jsx
@@ -1,3 +1,15 @@
-export const Footer = ({ label }) => {
-  return <div className="h-40 w-full bg-black text-white">{label}</div>;
+export const Footer = () => {
+  return (
+    <div className="h-40 w-full flex justify-evenly items-start py-14 bg-black text-white tablet:flex-col tablet:items-center tablet:gap-5">
+      <div className="flex justify-center items-center gap-5">
+        <p>PROJECTS</p>
+        <p>ABOUT ME</p>
+        <p>CONTACT</p>
+      </div>
+      <div className="flex flex-col justify-center items-start gap-2 tablet:items-center">
+        <p>jayceebautista0929@gmail.com</p>
+        <p>+63 912-3919-789</p>
+      </div>
+    </div>
+  );
 };
